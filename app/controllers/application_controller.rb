c44b2ef1aeca339bcc6e207aad479fec16395889
defo-wd <default.9110@gmail.com>
2023-09-22 07:49:51 +0000

    Login Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :profile_image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :profile_image])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "search" => "searches#search"
 
   # ユーザー関連のルート
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
